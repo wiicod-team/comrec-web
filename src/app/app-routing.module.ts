6896462ec1c3502b6e3c9116c29a574394935cbb
Edward NANDA <edward.nanda@wiicod.com>
2020-02-05 16:45:01 +0100

    Creation des pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {DashbaordComponent} from './dashbaord/dashbaord.component';
+import {AdministrationComponent} from './administration/administration.component';
+import {FactureListComponent} from './facture-list/facture-list.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : 'home', component : HomeComponent},
+  {path : 'facture', component : FactureListComponent},
+  {path : 'login', component : LoginComponent},
+  {path : 'admin', component : AdministrationComponent},
+  {path : 'dashboard', component : DashbaordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { FactureListComponent } from './facture-list/facture-list.component';
+import { DashbaordComponent } from './dashbaord/dashbaord.component';
+import { AdministrationComponent } from './administration/administration.component';
+import {NgChartjsModule} from 'ng-chartjs';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    FactureListComponent,
+    DashbaordComponent,
+    AdministrationComponent
   ],
   imports: [
     BrowserModule,
+    NgChartjsModule,
+    NgChartjsModule.registerPlugin(['line', 'pie', 'radar']),
     AppRoutingModule
   ],
   providers: [],
